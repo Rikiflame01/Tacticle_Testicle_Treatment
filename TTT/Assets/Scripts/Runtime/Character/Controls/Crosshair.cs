191a3fd1fb7f7f6b8dd8600b384e74d7197f7016
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2023-10-12 19:31:03 +0200

    FPS Changes

---
+++ TTT/Assets/Scripts/Runtime/Character/Controls/Crosshair.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    private Image crosshairImage;
+
+    private void Awake()
+    {
+        crosshairImage = GetComponent<Image>();
+    }
+
+    public void ShowCrosshair()
+    {
+        crosshairImage.enabled = true;
+    }
+
+    public void HideCrosshair()
+    {
+        crosshairImage.enabled = false;
+    }
+}
--- TTT/Assets/Scripts/Runtime/Character/Controls/MouseLook.cs
+++ TTT/Assets/Scripts/Runtime/Character/Controls/MouseLook.cs
@@ -5,17 +5,15 @@ public class MouseLook : MonoBehaviour
 {
     [SerializeField] private Transform playerCamera;
     [SerializeField] private float sensitivityX = 8f;
-    [SerializeField] private float sensitivityY = 0.5f;
     [SerializeField] private float xClamp = 85f;
     private float mouseX;
-    private float mouseY;
-    private float xRotation = 0f;
     private bool isPaused = false;
 
     private void Start()
     {
         SetCursorState();
         GameEvents.OnGamePaused += Pause;
+
         GameEvents.OnGameResumed += Resume;
     }
 
@@ -43,7 +41,6 @@ public class MouseLook : MonoBehaviour
     public void ReceiveInput(Vector2 _mouseInput)
     {
         mouseX = _mouseInput.x * sensitivityX;
-        mouseY = _mouseInput.y * sensitivityY;
     }
 
     private void Update()
@@ -52,11 +49,6 @@ public class MouseLook : MonoBehaviour
             return;
 
         transform.Rotate(Vector3.up * mouseX * Time.deltaTime);
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
-        Vector3 targetRotation = transform.eulerAngles;
-        targetRotation.x = xRotation;
-        playerCamera.eulerAngles = targetRotation;
     }
 
     private void Pause()
